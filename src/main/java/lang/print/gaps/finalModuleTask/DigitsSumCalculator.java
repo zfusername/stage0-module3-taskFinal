de81e7c1005a0f3c4e0d91de281be3a16f49e7f1
zfusername <farrukh_ziyaev@epam.com>
2023-07-20 14:53:58 +0500

    change

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -3,5 +3,6 @@ package lang.print.gaps.finalModuleTask;
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
         int sum = number % 1000 + (int)(number / 1000);
+        System.out.println(sum);
     }
 }
