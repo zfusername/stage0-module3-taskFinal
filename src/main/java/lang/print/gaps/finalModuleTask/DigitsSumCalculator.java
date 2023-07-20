cdb05c4f58738f6aafe55965a098c2fa1f09759a
zfusername <farrukh_ziyaev@epam.com>
2023-07-20 15:17:01 +0500

    change

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
-        int sum = number % 1000 + (int)(number / 1000);
+        int sum = number % 10 + (int)((number % 100) / 10) + (int)(number / 1000) + (int)((number % 1000) / 100);
         System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        double fahrenheitTemperature = (double) (temperatureCelsius * 9 / 5) + 32;
+        double fahrenheitTemperature = temperatureCelsius * 9.0 / 5.0 + 32;
         System.out.println(fahrenheitTemperature);
     }
 }
